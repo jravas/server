03f4b3064a66b6c53c98415ad54c615fc0eb7fdd
boletebrige <josip.ravas.broj@gmail.com>
2018-02-11 18:05:42 +0100

    users

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-const WebSocket = require('ws');
+import { connect } from 'net';
 
-const wss = new WebSocket.Server({ port: 8080 });
+const   WebSocket = require('ws');
+        wss = new WebSocket.Server({ port: 8080 });
+// lsit of users that are connected to the server
+var users = {};
 
 wss.on('connection', function connection(connection) {
   connection.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    var data;
+    try {
+        data = JSON.parse(message);
+    } catch (e) {
+        console.log('Error parsing JSON');
+        data = {};
+    }
+    // check if user is logged with taht id
+    switch (data.type) {
+        case 'login':
+            console.log('User logged in as ', data.name);
+            if (users[data.name]) {
+                sendTo(connection, {
+                    type: 'login',
+                    success: false
+                });
+            } else {
+                users[data.name] = connection;
+                connection.name = data.name;
+                sendTo(connection, {
+                    type: 'login',
+                    success: true
+                });
+            }
+            break;
+        default:
+        sendTo(connection, {
+            type: 'error',
+            message: 'Unrecognized command: ' + data.type
+        });
+        break;
+    }
   });
-
   connection.send('something');
+  // clean up clients connections when they disconenct
+  connection.on('close', function () {
+      if (connection.name) {
+          delete users[connection.name];
+      }
+  })
 });
+// handles sending a message to connection
+function sendTo(conn, message) {
+    conn.send(JSON.stringify(message));
+}
